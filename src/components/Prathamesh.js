fa6922ff25737b14050add072e6599ca8d3ea538
Prathamesh960 <prathameshbodake880@gmail.com>
2023-02-03 12:26:37 +0530

    prath

---
+++ src/components/Prathamesh.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Prathamesh(props) {
+    return (
+        <div>
+            <h1>Prathamesh Pune</h1>
+        </div>
+    );
+}
+
+export default Prathamesh;
