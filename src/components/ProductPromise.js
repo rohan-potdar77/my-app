a3f3bb1ba68ed7bfe39f3c2d6587ba84ecab6375
rohan <rohan.potdar77@gmail.com>
2023-02-10 15:56:27 +0530

    added product promise component 95% done

--- src/components/ProductPromise.js
+++ src/components/ProductPromise.js
@@ -6,24 +6,24 @@ export default function ProductPromise() {
     <>
       <section className='sec-1'>
         <div className='text'>
-          <h1>MAD RABBIT GOES BEYOND TATTOO AFTERCARE</h1>
+          <h2>MAD RABBIT GOES BEYOND TATTOO AFTERCARE</h2>
           <p>Delivering a clinical results-driven skincare line designed to improve overall skin quality with an environmentally-conscious approach. Our simple, universal products focus on the highest-quality, clean ingredients both natural and synthetic for uncompromising results.</p>
         </div>
       </section>
       <section className='sec-2'>
-        <h1 className='sec-2-1'>WHY IS TATTOO CARE IMPORTANT?</h1>
+        <h2 className='sec-2-1'>WHY IS TATTOO CARE IMPORTANT?</h2>
         <p className='sec-2-1'>It’s well known that the healing process post-tattoo can be pretty unpleasant: itchiness, inflammation, irritability, and flaking are just a few of the side effects you may experience. Incorporating a skin and body care routine that promotes soothing relief and skin barrier support, will not only help the healing process but keep your ink looking its best for longer.</p>
       </section>
       <section className='sec-3'>
         <div className='text'>
-          <h1>DO CLEAN INGREDIENTS MATTER?</h1>
+          <h2>DO CLEAN INGREDIENTS MATTER?</h2>
           <p>Short answer: Yes. The road to long-lasting ink begins with what you’re putting on your skin- from healing to daily care. Each ingredient should serve a purpose: soothe, moisturize, promote elasticity, help boost skin’s ability to regenerate and increase ink vibrancy and longevity. Clean ingredients are often better tolerated by even the most sensitive skin with many being biocompatible - aka easily recognized by skin. If it’s not providing a direct benefit, we don’t want it.</p>
         </div>
       </section>
       <section className='sec-4'>
         <div className='center'>
-          <h1>THE MRT DIFFERENCE:
-            FORMULATED FOR HEALTHY SKIN AND VIBRANT TATTOOS</h1>
+          <h2>THE MRT DIFFERENCE:
+            FORMULATED FOR HEALTHY SKIN AND VIBRANT TATTOOS</h2>
         </div>
         <div className='img-con'>
           <img className='img-item' src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/Group_11_a8e81833-7700-4da0-ba0d-8afcfd79acea.png?v=1650352494'></img>
@@ -48,7 +48,7 @@ export default function ProductPromise() {
         </div>
       </section>
       <section className='sec-6'>
-      <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_02364_1.png?v=1650341849' className='box-1'></img>
+        <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_02364_1.png?v=1650341849' className='box-1'></img>
         <div className='box-2'>
           <h3><strong>VITAMIN C</strong></h3>
           <p>Potent antioxidant that visibly brightens and helps support natural collagen production for a firmer, more youthful appearance to skin</p>
@@ -62,7 +62,7 @@ export default function ProductPromise() {
         </div>
       </section>
       <section className='sec-7'>
-      <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_05236_2.png?v=1650344872' className='box-1'></img>
+        <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_05236_2.png?v=1650344872' className='box-1'></img>
         <div className='box-2'>
           <h3><strong>DEIONIZED WATER</strong></h3>
           <p>Purified water that’s gone through a filtration process to remove minerals, pollutants and microbes that can compromise formula efficacy and stability. Also called demineralized water</p>
@@ -76,7 +76,81 @@ export default function ProductPromise() {
         </div>
       </section>
       <section className='sec-8'>
-
+        <div>
+          <h3>MAD RABBIT MAKES AN IMPACT STRIVING TOWARDS SUSTAINABILITY</h3>
+          <p>MRT packaging is made with fully-recyclable materials.</p>
+          <p>In April 2021 Mad Rabbit became a carbon neutral company through the acquisition of carbon offsets. As an all-natural aftercare company, we never test our products on animals and are 100% cruelty-free.</p>
+          <p>We believe our impact goes beyond the products we produce and advocate for leaving the world a little bit better than when we started. In using carbon offsets, we support specific initiatives globally to offset carbon through sustainable practices. Developing countries often do not have the means to support environmentally friendly initiatives, so we decided to step up.</p>
+        </div>
+      </section>
+      <section className='sec-9'>
+        <h3>In partnership with Neutrl, to-date Mad Rabbit has proudly:</h3>
+        <div className='img-cont'>
+          <div className='in-div'>
+            <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_09884_1_1.png?v=1650371311' className='my-img'></img>
+            <div className='ins-div'>
+              <h3>33,450</h3>
+              <p>CARBON OFFSETS CREATED</p>
+            </div>
+          </div>
+          <div className='in-div'>
+            <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_05236_1_a09a5a68-7c6e-47f8-bc8f-442cf0eeef04.png?v=1650374429' className='my-img'></img>
+            <div className='ins-div'>
+              <h3>965,500</h3>
+              <p>POUNDS OF CO2 REMOVED</p>
+            </div>
+          </div>
+          <div className='in-div'>
+            <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/Screen_Shot_2022-02-18_at_9.13_1_1.png?v=1650371410' className='my-img'></img>
+            <div className='ins-div'>
+              <h3>438.9</h3>
+              <p>TONS OF CO2 REMOVED THANKS TO OUR CUSTOMERS</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='sec-10'>
+        <img src='https://cdn.shopify.com/s/files/1/0076/4156/3234/files/GC_06291_1.png?v=1650370638' className='box-1'></img>
+        <div className='box-2'>
+          <h3>CAUSES</h3>
+          <ul>
+            <li>Population Growth</li>
+            <li>Agriculture</li>
+            <li>Deforestation</li>
+            <li>Transportation (cars, airplanes, ships)</li>
+            <li>Increased energy consumption</li>
+            <li>Living standards</li>
+            <li>Waste disposal</li>
+            <li>Lack of education and more…</li>
+          </ul>
+          <h3>HOW TO OFFSET</h3>
+          <ul>
+            <li>Public transportation vs cars</li>
+            <li>Saving energy</li>
+            <li>Cutting down on meat consumption</li>
+            <li>Recycling and upcycling</li>
+            <li>Energy efficiency home upgrades</li>
+            <li>Shopping locally when possible</li>
+            <li>Educating yourself</li>
+          </ul>
+        </div>
+      </section>
+      <section className='sec-11'>
+        <div>
+          <h3>HOW CAN THE MRT COMMUNITY HELP?</h3>
+          <ul>
+            <li>Recycle your empty products</li>
+            <li>Carpool or take public transportation to tattoo sessions</li>
+            <li>Use biodegradable paper towels when cleaning fresh ink</li>
+            <li>Focus on energy-saving best practices at home</li>
+          </ul>
+        </div>
+      </section>
+      <section className='sec-12'>
+        <h3>Let’s keep in touch</h3>
+        <p>Sign up for Mad Rabbit Newsletter and get all the promotions and news in your inbox.</p>
+        <input type='email' placeholder='Enter Your Email'></input>
+        <input type='button' value='Subscribe'></input>
       </section>
     </>
   )
